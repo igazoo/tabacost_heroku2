fa5460f516bd63e6c057f1f99b84364b250275eb
nao <ec2-user@ip-172-31-30-122.us-east-2.compute.internal>
2019-10-16 06:50:33 +0000

    weekdata

--- app/controllers/histories_controller.rb
+++ app/controllers/histories_controller.rb
@@ -1,47 +1,86 @@
 class HistoriesController < ApplicationController
+  def index
+    array1=[]
+    #日ごとのデータを取得
+    array1=current_user.histories.all.map{ |history| history.created_at.day }
     
-    def index
-        array1=[]
-        #日ごとのデータを取得
-        
-        array1=current_user.histories.all.map{ |history| history.created_at.day } 
-  
-        #重複しているものを数える（同じ日のデータを配列にする）
-        dayArray = array1.group_by(&:itself)
-        
-         logger.debug(array1.inspect)
-        #1日のデータをまとめて各日のデータを連想配列に入れる
-        dayList = {}
+    logger.debug(array1)
+    #重複しているものを数える（同じ日のデータを配列にする）
+    dayArray = array1.group_by(&:itself)
+    #logger.debug(["★dayArray"=>dayArray])
+    #logger.debug(["★array1"=>array1.inspect])
+    #1日のデータをまとめて各日のデータを連想配列に入れる
+    dayList = {}
       
-        dayArray.each do |k,v|
-            
-          
-          dayList[k] = v.count
-          
-          
-        end
-       @chart1= dayList
-        
-        
-       
-    
-        
-         array2=[]
-        array2=current_user.histories.all.map{ |history| history.created_at.month } 
-  
-        dayArray = array2.group_by(&:itself)
-  
-        monthList = {}
-      
-        dayArray.each do |k,v|
-          
-          monthList[k] = v.count
-              
-        end
-        @chart2= monthList
-     
-    
+    dayArray.each do |k,v|
+      dayList[k] = v.count
     end
+    
+    @chart1= dayList
+    array2=[]
+    array2=current_user.histories.all.map{ |history| history.created_at.month } 
+  
+    dayArray = array2.group_by(&:itself)
+  
+    monthList = {}
+      
+    dayArray.each do |k,v|
+      monthList[k] = v.count
+    end
+    
+    
+    histories = current_user.histories.all
+    day_count = histories.first.created_at
+    
+    #二次元配列の初期化
+    tmp = Array.new
+    data = Array.new
+    data.push(tmp)
+    
+    i = 0
+    
+
+    histories.each do |h|
+      if h.created_at > day_count + 1.week
+        logger.debug('---------------------１週間---------------------')
+        #debugger
+        day_count = day_count + 1.week
+        i += 1
+        data.push(Array.new)
+      end
+      data[i].push(h.created_at)
+      logger.debug(['push'=>h.created_at])
+    end
+    logger.debug('---------------------------------------------')
+    
+    data.each_with_index do |d,i|
+      logger.debug(["data[#{i}]"=>d])
+    end
+    logger.debug('---------------------------------------------')
+    logger.debug(['data'=>data])
+
+    
+    @chart2= monthList
+  end
+  
+  
+  def index2
+    histories = current_user.histories.all
+    day_count = histories.first.created_at
+    histories_in_a_week = Array.new
+    data = Array.new
+    
+    histories.each do |h|
+      if h.created_at < day_count + 7
+        histories_in_a_week.push(h)
+      else
+        day_count += 7
+        data.push(histories_in_a_week)
+        histories_in_a_week.clear
+      end
+    end
+    logger.debug(['★data'=>data])
+  end
   
    def new
        
